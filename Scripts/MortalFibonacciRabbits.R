36683aaa8b559e1a738334f74c0114212f011462
klterwelp <klterwelp@gmail.com>
2022-12-19 13:04:03 -0500

    Update MortalFibonacciRabbits.R
    fixed mortal fib rabbits

--- Scripts/MortalFibonacciRabbits.R
+++ Scripts/MortalFibonacciRabbits.R
@@ -1,19 +1,27 @@
 # Mortal Fibonacci Rabbits
-## Given: positive itnergers n<= 100 m<= 20 
+## Given: positive intergers n<= 100 m<= 20 
 ## Return: Total number of rabbit pairs after n-th month if all rabbits live m months. 
 
 n <- 6
 m <- 3
 
-rabbits <- c(1,1)
+dead <- c(0,0)
+adults <- c(0,1)
+babies <- c(1,0)
+total <- c(1,1)
 
-for (i in 3:n) {
-  if (i <= m ) {
-    rabbits[i] <- rabbits[i-1]+rabbits[i-2] 
-    print(rabbits[i]) } else {
-      rabbits[i] <- rabbits[i-1]+rabbits[i-2]-rabbits[i-m]
-      print(rabbits[i])
-    }
-}
 
-rabbits[n]
+for(i in 3:(m)) { 
+  dead[i] <- 0
+  babies[i] <- adults[i-1]
+  adults[i] <- adults[i-1]+babies[i-1]
+  total[i] <- adults[i]+babies[i]} 
+
+for(i in (m+1):n) {
+  dead[i] <- babies[i-m]
+  babies[i] <- adults[i-1]
+  adults[i] <- adults[i-1]+babies[i-1]-dead[i]
+  total[i]<-adults[i]+babies[i]}
+    
+
+total[n]
